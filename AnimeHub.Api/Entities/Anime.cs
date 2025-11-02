3399c8eabcc3a6ebec0fd184cb26413088da9101
Brandon Hill <brandonjon26@gmail.com>
2025-11-02 12:09:03 -0600

    Feature: Complete EF Core setup; remove development appsettings file from tracking

---
+++ AnimeHub.Api/Entities/Anime.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnimeHub.Api.Entities
+{
+    [Table("Anime")]
+    public class Anime
+    {
+        [Key]
+        public long AnimeId { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+
+    }
+}
