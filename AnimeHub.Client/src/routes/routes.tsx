d435d8cc00a0c949c6367e5a88b4a8433a392314
Brandon Hill <brandonjon26@gmail.com>
2025-11-02 01:12:20 -0500

    Feature: Added Ayami Avatar and her about page to the app

--- AnimeHub.Client/src/routes/routes.tsx
+++ AnimeHub.Client/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import React from "react";
 import HomePage from "../features/home/HomePage";
+import AboutAyamiPage from "../features/ayami/AboutAyamiPage";
 
 // The array that defines all routes in the application
 const router = createBrowserRouter([
@@ -9,6 +10,12 @@ const router = createBrowserRouter([
     element: <HomePage />, // The entry point of the application
     errorElement: <div>Oops! Something went wrong.</div>, // Custom error boundary
     // loader: rootLoader, // Add data loading functions here later
+  },
+  {
+    // 🔑 New Route for the Ayami Page
+    path: "/ayami",
+    element: <AboutAyamiPage />,
+    errorElement: <div>Oops! Ayami seems to have wandered off!</div>,
   },
   // We will add other feature routes here (e.g., /login, /anime/:id)
 ]);
