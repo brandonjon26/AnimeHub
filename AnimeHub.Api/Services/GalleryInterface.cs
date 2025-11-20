557f170ef6ee82fd2d74aa09386ae4b9f8a9cd39
Brandon Hill <brandonjon26@gmail.com>
2025-11-19 19:17:36 -0600

    Feature: added PUT endpoint for single images

--- AnimeHub.Api/Services/GalleryInterface.cs
+++ AnimeHub.Api/Services/GalleryInterface.cs
@@ -5,12 +5,20 @@ namespace AnimeHub.Api.Services
 {
     public interface GalleryInterface
     {
+        // READ Operations
         Task<IEnumerable<GalleryImageDto>> GetFeaturedImagesAsync();
         Task<IEnumerable<GalleryImageCategoryDto>> GetAllCategoriesAsync();
         Task<IEnumerable<GalleryImageDto>> GetImagesByCategoryNameAsync(string categoryName, bool isAdult);
+
+        // CREATE Operations
         Task<int> CreateImageBatchAsync(GalleryImageCreateBatchDto dto, IFormFile[] files);
         Task<GalleryImageDto?> CreateSingleImageAsync(GalleryImageCreateSingleDto dto);
+
+        // UPDATE Operations
         Task<bool> UpdateGalleryFolderAsync(int categoryId, GalleryImageUpdateFolderDto dto);
+        Task<bool> UpdateSingleImageAsync(long imageId, GalleryImageUpdateSingleDto dto);
+
+        // DELETE Operations
         Task<bool> DeleteGalleryFolderAsync(int categoryId);
         Task<bool> DeleteImageAsync(long imageId);
         Task<bool> DeleteCategoryAsync(int categoryId);
