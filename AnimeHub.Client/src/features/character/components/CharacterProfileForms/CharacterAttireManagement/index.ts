a7e9f91777d70d6dba42e4f51c02a7d8b5570bb0
Brandon Hill <brandonjon26@gmail.com>
2025-12-19 23:54:48 -0600

    Restructured CharacterProfileForms folder; migrated CharacterProfileUpdateForm to dedicated utils & hook files

---
+++ AnimeHub.Client/src/features/character/components/CharacterProfileForms/CharacterAttireManagement/index.ts
@@ -0,0 +1,1 @@
+export { default as CharacterAttireManagement } from "./CharacterAttireManagement";
---
+++ AnimeHub.Client/src/features/character/components/CharacterProfileForms/CharacterProfileUpdateForm/index.ts
@@ -0,0 +1,1 @@
+export { default as CharacterProfileUpdateForm } from "./CharacterProfileUpdateForm";
