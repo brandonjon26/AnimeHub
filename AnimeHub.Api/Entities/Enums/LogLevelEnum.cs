da4db209bcf2374fa18ff3d3eb17f4c9abdb5915
Brandon Hill <brandonjon26@gmail.com>
2026-01-24 17:14:20 -0600

    Configured Serilog

--- AnimeHub.Api/Entities/Enums/LogLevelEnum.cs
+++ AnimeHub.Api/Entities/Enums/LogLevelEnum.cs
@@ -2,9 +2,13 @@
 {
     public enum LogLevel
     {
-        Information = 0,
-        Warning = 1,
-        Error = 2,
-        Critical = 3
+        None = 0,
+        Verbose = 1,
+        Debug = 2,
+        Information = 3,
+        Warning = 4,
+        Error = 5,
+        Fatal = 6,
+        Critical = 7
     }
 }
--- AnimeHub.Api/Infrastructure/Logging/LoggerExtensions.cs
+++ AnimeHub.Api/Infrastructure/Logging/LoggerExtensions.cs
@@ -1,6 +1,44 @@
-﻿namespace AnimeHub.Api.Infrastructure.Logging
+﻿using AnimeHub.Api.Entities.Enums;
+using Serilog.Context;
+using Serilog.Core.Enrichers;
+
+namespace AnimeHub.Api.Infrastructure.Logging
 {
-    public class LoggerExtensions
+    public static class LoggerExtensions
     {
+        /// <summary>
+        /// Wraps a logging operation with the necessary context for the AnimeHub Dashboard.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="source"></param>
+        /// <param name="userId"></param>
+        /// <param name="traceId"></param>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        public static IDisposable BeginPropertyScope(this ILogger logger, LogSource source, string? userId = null, string? traceId = null, string? payload = null)
+        {
+            // Push each property individually and store their disposables
+            var disposables = new List<IDisposable>
+            {
+                LogContext.PushProperty("LogSourceId", (int)source),
+                LogContext.PushProperty("UserId", userId ?? string.Empty),
+                LogContext.PushProperty("TraceId", traceId ?? string.Empty),
+                LogContext.PushProperty("Payload", payload ?? string.Empty)
+            };
+
+            return new CompositeDisposable(disposables);
+        }
+
+        // A simple helper to dispose of multiple items in one go
+        private class CompositeDisposable(IEnumerable<IDisposable> disposables) : IDisposable
+        {
+            public void Dispose()
+            {
+                foreach (var disposable in disposables)
+                {
+                    disposable.Dispose();
+                }
+            }
+        }
     }
 }
