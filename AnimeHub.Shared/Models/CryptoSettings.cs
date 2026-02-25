1179e3fd4b3c26609c54964f0032a0f4a836b4a6
Brandon Hill <brandonjon26@gmail.com>
2026-02-25 09:49:21 -0600

    Cleaned up obfuscate/deobfuscate code

---
+++ AnimeHub.Shared/Models/CryptoSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeHub.Shared.Models
+{
+    public class CryptoSettings
+    {
+        public string Key { get; set; } = string.Empty;
+    }
+}
