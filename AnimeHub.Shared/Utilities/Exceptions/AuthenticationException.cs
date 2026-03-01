f86efd5f69dcc61bef35728019763ab9d72f9dde
Brandon Hill <brandonjon26@gmail.com>
2026-02-28 22:32:55 -0600

    Added custom exception classes

---
+++ AnimeHub.Shared/Utilities/Exceptions/AuthenticationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnimeHub.Shared.Enums;
+
+namespace AnimeHub.Shared.Utilities.Exceptions
+{
+    public class AuthenticationException : AnimeHubException
+    {
+        public AuthenticationException(string message, object? payload = null)
+            : base(message, 401, payload, LogLevel.Warning, LogSource.Security) { }
+    }
+}
