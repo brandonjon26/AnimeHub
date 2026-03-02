6008f0bafb2fa14f40010e13cc9ad5da13eebf90
Brandon Hill <brandonjon26@gmail.com>
2026-03-02 15:08:55 -0600

    Added more custom exception classes

---
+++ AnimeHub.Shared/Utilities/Exceptions/DuplicateDataExceptions/AnimeAlreadyExistsException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace AnimeHub.Shared.Utilities.Exceptions.DuplicateDataExceptions
+{
+    public class AnimeAlreadyExistsException : ConflictException
+    {
+        public AnimeAlreadyExistsException(string message, object? payload = null) 
+            : base(message, payload) { }
+    }
+}
