a43b7895dae36f387c3a27c5b5a894b3ae0f9d1a
Brandon Hill <brandonjon26@gmail.com>
2026-03-05 22:52:04 -0600

    Added error handling and logging to UserProfileService

---
+++ AnimeHub.Shared/Utilities/Exceptions/UserProfileNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnimeHub.Shared.Enums;
+
+namespace AnimeHub.Shared.Utilities.Exceptions
+{
+    public class UserProfileNotFoundException : AnimeHubException
+    {
+        public UserProfileNotFoundException(string message, object? payload = null, Exception? innerException = null) 
+        : base(message, 404, payload, innerException, LogLevel.Warning, LogSource.WebAPI) { }
+    }
+}
