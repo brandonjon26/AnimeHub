51eafa97fb0c434593b42d788bed04d07aec3b4d
Brandon Hill <brandonjon26@gmail.com>
2026-03-12 10:29:54 -0500

    Small UserProfileService corrections; stated on character validators

---
+++ AnimeHub.Api/Infrastructure/Validators/Character/CharacterProfileUpdateValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using AnimeHub.Api.DTOs.Character;
+
+namespace AnimeHub.Api.Infrastructure.Validators.Character
+{
+    public class CharacterProfileUpdateValidator : AbstractValidator<CharacterProfileUpdateDto>
+    {
+        public CharacterProfileUpdateValidator()
+        {
+            // Rules will live here
+        }
+    }
+}
