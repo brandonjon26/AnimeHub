709ca7882f410f96d0538e9d9f182f1909729b3b
Brandon Hill <brandonjon26@gmail.com>
2026-03-13 15:04:46 -0500

    Finished setting up character validators

---
+++ AnimeHub.Api/Infrastructure/Validators/Character/CharacterAccessoryInputValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using AnimeHub.Api.DTOs.Character;
+
+namespace AnimeHub.Api.Infrastructure.Validators.Character
+{
+    public class CharacterAccessoryInputValidator : AbstractValidator<CharacterAccessoryInputDto>
+    {
+        public CharacterAccessoryInputValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().MaximumLength(500);
+            RuleFor(x => x.UniqueEffect).MaximumLength(500).When(x => !string.IsNullOrEmpty(x.UniqueEffect));
+        }
+    }
+}
